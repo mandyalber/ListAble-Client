3c50e1ce4d969ab63acd0096af95c4223d0e98dc
Mandy Alber <mandy.alber@gmail.com>
2021-01-14 13:39:21 -0800

    update to production endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    //API_ENDPOINT: `https://quiet-stream-59097.herokuapp.com/api/`,
-    API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: `https://quiet-stream-59097.herokuapp.com/api/`,
+    //API_ENDPOINT: 'http://localhost:8000/api'
   }
   
