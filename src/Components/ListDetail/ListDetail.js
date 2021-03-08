3feee8eb8dbc9ce555aeb61828928bd535ab7fd8
Mandy Alber <mandy.alber@gmail.com>
2021-03-08 14:14:56 -0800

    added dnd to list items

--- src/Components/ListDetail/ListDetail.js
+++ src/Components/ListDetail/ListDetail.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import ListContext from '../ListContext'
 import AddItem from './AddItem'
 import Item from './Item'
@@ -8,14 +9,34 @@ import './ListDetail.css'
 //displays selected list along with category it belongs to, items associated
 export default function ListDetail({ match }) {
 
-    const { listState, itemState, categoryState } = React.useContext(ListContext)
+    const { listState, itemState, categoryState, setItemState } = React.useContext(ListContext)
 
     const list = listState.filter(list => list.id === parseInt(match.params.listId)).pop() || { id: null }
 
     const category = categoryState.filter(cat => cat.id === parseInt(list.categoryId)).pop() || { id: null }
 
     const items = itemState.length ? itemState.filter(item => item.listId === list.id)
-        .map(item => <li key={item.id}><Item {...item} /></li>) : ''
+        .map((item, index) =>
+            <Draggable key={item.id} draggableId={item.name} index={index}>
+                {(provided) => (
+                    <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <Item {...item} />
+                    </li>
+                )}
+            </Draggable>) : ''
+
+    const handleOnDragEnd = (result) => {
+        if (!result.destination) {
+            return
+        }
+        const items = Array.from(itemState)
+        const [reorderedItem] = items.splice(result.source.index, 1)
+
+        items.splice(result.destination.index, 0, reorderedItem)
+
+        setItemState(items)
+        console.log(itemState)
+    }
 
     if (list.id) {
         return (
@@ -25,7 +46,19 @@ export default function ListDetail({ match }) {
                 <div className="list">
                     <h2>{list.name}</h2>
                     <AddItem listId={list.id} />
-                    <ul className="todo">{items.length ? items.reverse() : ''}</ul>
+                    <DragDropContext onDragEnd={handleOnDragEnd}>
+                        <Droppable droppableId="list-items">
+                            {(provided) => (
+                                <ul
+                                    className="todo"
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}>
+                                    {items.length ? items : ''}
+                                    {provided.placeholder}
+                                </ul>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
                 </div>
             </main>
         )
